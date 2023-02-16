8cf6d6d393a8c0150e5291ea6644ee8eca660a79
Bogdan Markov <bogdick@yandex.ru>
2023-02-16 23:51:54 +0100

    Troll face catches coins!

---
+++ src/main.js
@@ -0,0 +1,85 @@
+const WIDTH = 800;
+const HEIGHT = 600;
+
+const config = {
+    type: Phaser.AUTO,
+    width: WIDTH,
+    height: HEIGHT,
+    backgroundColor: '#0980c6',
+    physics: {
+        default: 'arcade',
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update,
+    }
+};
+
+const game = new Phaser.Game(config);
+
+function preload () {
+    this.load.image('troll-default', 'res/troll-default.webp');
+    this.load.image('troll-problem', 'res/troll-problem.webp');
+    this.load.image('troll-crazy', 'res/troll-crazy.jpg');
+    this.load.image('coin', 'res/coin.png');
+}
+
+function create () {
+    this.cursors = this.input.keyboard.createCursorKeys();
+
+    let coins = [];
+    for (let i = 0; i < 5; i++) {
+        let x = Math.floor(Math.random() * WIDTH) + 1;
+        let y = Math.floor(Math.random() * HEIGHT) + 1;
+        let coin = this.physics.add.image(x, y, 'coin');
+        coin.setScale(0.1);
+        // coin.setBounce(100, 100);
+        coin.setCollideWorldBounds(true);
+        coins.push(coin);
+    }
+    this.coins = coins;
+
+    let troll = this.physics.add.image(WIDTH / 2, HEIGHT / 2, 'troll-default');
+    troll.setScale(0.5);
+    // troll.setBounce(1, 1);
+    troll.setCollideWorldBounds(true);
+    this.troll = troll
+
+    for (let i = 0; i < 5; i++) {
+        let coin = coins[i];
+        this.physics.add.overlap(troll, coin, grabCoin);
+    }
+}
+
+function update() {
+    let cursors = this.cursors
+    let troll = this.troll
+
+    if (cursors.left.isDown) {
+        troll.setVelocityX(-300);
+    } else if (cursors.right.isDown) {
+        troll.setVelocityX(300);
+    } else {
+        troll.setVelocityX(0);
+    }
+
+    if (cursors.up.isDown) {
+        troll.setVelocityY(-300);
+    } else if (cursors.down.isDown) {
+        troll.setVelocityY(300);
+    } else {
+        troll.setVelocityY(0);
+    }
+}
+
+function grabCoin(troll, coin) {
+    // coin.destroy();
+
+    let distance = Phaser.Math.Distance.Between(troll.x, troll.y, coin.x, coin.y);
+    if (distance < coin.width / 30) {
+        let x = Math.floor(Math.random() * WIDTH) + 1;
+        let y = Math.floor(Math.random() * HEIGHT) + 1;
+        coin.setPosition(x, y);
+    }
+}
